54f46eb029034c5e81f9ccb762071003d9143c2a
William.Chiang <william.chiang@innotech.me>
2023-11-28 18:43:48 +0800

    feat: solid page test 自動帶入router

--- utilities/templates/solid/test-template.js
+++ utilities/templates/solid/test-template.js
@@ -10,11 +10,15 @@ const getTemplate = ({ name, isPage, testTool }) => {
     testTool === "vitest"
       ? "import { describe, it, expect } from 'vitest';\n"
       : ""
+  }${
+    isPage && "import { Router } from '@solidjs/router';\n"
   }import { render, screen } from '@solidjs/testing-library';`;
   const TestTarget = `import ${isPage ? "Page" : "Component"} from '.';`;
-  const BaseUITest = `it('Should render',() => {render(() => <${
-    isPage ? "Page" : `Component testId = "${name}"`
-  } />\n);expect(screen.getByTestId('${name}')).not.toBeNull();\n});`;
+  const BaseUITest = `it('Should render',() => {render(() => ${
+    isPage && "<Router>"
+  }<${isPage ? "Page" : `Component testId = "${name}"`} />${
+    isPage && "</Router>"
+  }\n);expect(screen.getByTestId('${name}')).not.toBeNull();\n});`;
 
   return `${TestLibrary}\n\n${TestTarget}\n\ndescribe('UI test', () => {\n${BaseUITest}\n });`;
 };

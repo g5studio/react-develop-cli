c176e2146c4aa752b48ef61d4caa4cdf246aab0b
William.Chiang <william.chiang@innotech.me>
2024-01-16 22:54:47 +0800

    feat: update solid model template

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ process.env["root"] = `../${process
 program
   .name("g5-toolbox-cli")
   .description("CLI for quick build g5 pattern project")
-  .version("1.1.4");
+  .version("1.1.5");
 
 program
   .command("set")

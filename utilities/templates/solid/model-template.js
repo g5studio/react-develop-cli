fc5f110bd23dab876798663c4b456eb866dd2c12
William.Chiang <william.chiang@innotech.me>
2024-01-16 22:54:19 +0800

    feat: update solid model template

--- utilities/templates/solid/model-template.js
+++ utilities/templates/solid/model-template.js
@@ -11,14 +11,9 @@ const getTemplate = (name) => {
   `;
   const RelativeModelTemplate = `
   export const get${name} = (): I${name} => {
-    const initialData: ${name} = {} as ${name};
-    const [metaData, setData] = createStore<${name}>(initialData);
-  
-    // TODO mapping your api data here , you should reassign the value event if the field name are the same between api schema and model interface , please remove this line before commit 
-    const initialize = (apiResponse: unknown) => {
-      setData({});
-    };
-  
+    const initialData = (): ${name} => ({} as ${name});
+    const [metaData, setData] = createStore<${name}>(initialData());
+
     // !no need to change
     const updateData = (data: Partial<${name}>) => {
       setData(data);
@@ -27,7 +22,12 @@ const getTemplate = (name) => {
   
     // !no need to change
     const reset = () => {
-      setData(reconcile(initialData));
+      setData(reconcile(initialData()));
+    };
+  
+    // TODO mapping your api data here , you should reassign the value event if the field name are the same between api schema and model interface , please remove this line before commit 
+    const initialize = (apiResponse: unknown) => {
+      updateData({});
     };
   
     return { metaData, updateData, initialize, reset };
